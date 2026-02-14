78ae96a765e3c745cb18ec648bf9c3453543e11a
rasult22 <warrior.006@inbox.ru>
2026-02-14 12:54:55 +0500

    ci: configure deployment to GitHub Pages
    - Update site URL and base path i

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://principleplanner.app',
+  site: 'https://rasult22.github.io',
+  base: '/covey-landing',
   integrations: [
     tailwind({
       applyBaseStyles: false,
